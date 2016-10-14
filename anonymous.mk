695d959abbb0fbc159b6e97877a5596bba2c982d
lo52tp <lo52tp@lo52tp-VirtualBox.(none)>
2016-10-14 14:56:02 +0200

    modification de la bootanimation

--- anonymous.mk
+++ anonymous.mk
@@ -2,6 +2,8 @@ $(call inherit-product, device/linaro/pandaboard/pandaboard.mk)
 
 DEVICE_PACKAGE_OVERLAYS:=\
     device/utbm/anonymous/overlay
+PRODUCT_COPY_FILES += \
+	device/utbm/anonymous/system/media/bootanimation.zip:/system/media/bootanimation.zip
 
 PRODUCT_NAME:= anonymous
 PRODUCT_DEVICE:= anonymous
